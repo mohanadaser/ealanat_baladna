e5d264f556716dc9e9b0fbf3743995401bd44675
mohanad <mohanadhassany4@gmail.com>
2024-08-08 00:06:23 +0300

    state

--- lib/views/admin_panel/home_admin.dart
+++ lib/views/admin_panel/home_admin.dart
@@ -9,14 +9,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-class HomeAdmin extends StatefulWidget {
+class HomeAdmin extends StatelessWidget {
   const HomeAdmin({super.key});
 
-  @override
-  State<HomeAdmin> createState() => _HomeAdminState();
-}
-
-class _HomeAdminState extends State<HomeAdmin> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(
